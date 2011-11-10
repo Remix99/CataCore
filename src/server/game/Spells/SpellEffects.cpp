e6d4a8803260eb59e3327619ed667552705207f4
Remix CataWoW <remixcata@live.com>
2011-11-09 22:21:51 -0600

    Fixed InnerWill ShadowOrbs Evangalism

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -621,18 +621,55 @@ void Spell::SpellDamageSchoolDmg(SpellEffIndex effIndex)
             case SPELLFAMILY_PRIEST:
             { 
               // Evangelism 
-                if (m_caster->HasAura(81659)) // Rank 1
+                switch (m_spellInfo->Id)
+                {
+                    case 73413:  // inner will
+                        m_caster->RemoveAurasDueToSpell(588);
+                        break;
+                    case 588:    // inner fire
+                        m_caster->RemoveAurasDueToSpell(73413);
+                         break;
+                }
+
+                if (m_spellInfo->Id == 589 || m_spellInfo->Id == 15407)  //Shadow Word: Pain | mind flay
+                {
+                    if (m_caster->HasSpell(95740))   // Shadow Orbs
+                    {
+                        int chance = 10;
+
+                        if (m_caster->HasAura(33191)) // Harnessed Shadows rank1
+                            chance += 4;
+                        else if(m_caster->HasAura(78228))  // Harnessed Shadows rank2
+                            chance += 8;
+
+                        if (roll_chance_i(chance))
+                            m_caster->CastSpell(m_caster, 77487, true);
+                    }
+                }
+
+                if (m_caster->HasAura(81659)) // Evangelism Rank 1
                 { 
-                    if (m_spellInfo->Id == 585)
+                    if (m_spellInfo->Id == 585 || m_spellInfo->Id == 14914 || m_spellInfo->Id == 15407)  // Smite | Holy Fire | mind flay
                         m_caster->CastSpell(m_caster,81660,true);
                 }
-                else
                  
-                if (m_caster->HasAura(81662)) // Rank 2
+                if (m_caster->HasAura(81662)) // Evangelism Rank 2
                 {
-                    if (m_spellInfo->Id == 585)
+                    if (m_spellInfo->Id == 585 || m_spellInfo->Id == 14914 || m_spellInfo->Id == 15407)  // Smite | Holy Fire | mind flay
                         m_caster->CastSpell(m_caster,81661,true);
                 }
+				//Dark Evangelism  not implemented yet
+                if (m_caster->HasAura(81659))                     // Evangelism rank 1
+                {
+                    if (m_spellInfo->Id == 15407)                   //  Mind Flay
+                        m_caster->CastSpell(m_caster,87117,true);   // Dark Evangelism
+                }
+                else if (m_caster->HasAura(81662))                  // Evangelism rank 2
+                {
+                    if (m_spellInfo->Id == 15407)                   //  Mind Flay
+                        m_caster->CastSpell(m_caster,87118,true);   // Dark Evangelism
+                }
+                break;
 
                 // Chakra
                 if (m_caster->HasAura(14751))
