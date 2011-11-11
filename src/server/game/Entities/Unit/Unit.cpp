3c4522b3cc3b169574c676720994822201319863
Remix CataWoW <remixcata@live.com>
2011-11-10 23:12:23 -0600

    reverted eye for an eye

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -5111,16 +5111,7 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, AuraEffect* trigger
                 case 25988:
                 {
                     // return damage % to attacker but < 50% own total health
-                    basepoints0 = 5*int32(damage)/100;
-                    if (basepoints0 > GetMaxHealth()/2)
-                        basepoints0 = GetMaxHealth()/2;
-
-                    triggered_spell_id = 25997;
-
-					break;
-                    // return damage % to attacker but < 50% own total health
-                    // basepoints0 = int32((triggerAmount * damage) /100);
-          basepoints0 = 10*int32(damage)/100;
+                    basepoints0 = int32((triggerAmount * damage) /100);
                     int32 halfMaxHealth = int32(CountPctFromMaxHealth(50));
                     if (basepoints0 > halfMaxHealth)
                         basepoints0 = halfMaxHealth;
@@ -6709,12 +6700,9 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, AuraEffect* trigger
                 // Judgements of the Bold
                 case 89901:
                 {
-          if (roll_chance_f(triggerAmount) && !this->IsWithinDistInMap(pVictim, 15.0f))
-          {
-            target = this;
-            triggered_spell_id = 89906;
-            break;
-          }
+                target = this;
+                triggered_spell_id = 89906;
+                break;
                 }
                     break;
 			    // Ancient Healer
