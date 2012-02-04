41cf8b05c47618ab3bc3ef145b27750912c66a00
Remix CataWoW <remixcata@live.com>
2012-02-04 00:54:24 -0600

    Fixed infinite duration

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -3508,6 +3508,12 @@ void Spell::handle_immediate()
             m_caster->AddInterruptMask(m_spellInfo->ChannelInterruptFlags);
             SendChannelStart(duration);
         }
+    else if (duration == -1)
+    {
+      m_spellState = SPELL_STATE_CASTING;
+      m_caster->AddInterruptMask(m_spellInfo->ChannelInterruptFlags);
+      SendChannelStart(duration);
+    }
     }
 
     PrepareTargetProcessing();
