b245eeada091eaca396ccc52d0071e1a63947379
Remix CataWoW <remixcata@live.com>
2012-02-03 21:09:20 -0600

    Fix spell Bloodthirst

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -520,8 +520,10 @@ void Spell::SpellDamageSchoolDmg(SpellEffIndex effIndex)
             case SPELLFAMILY_WARRIOR:
             {
                // Bloodthirst
-               if (m_spellInfo->SpellFamilyFlags[1] & 0x400)
-                   damage = uint32(damage * (m_caster->GetTotalAttackPowerValue(BASE_ATTACK)) / 100);
+            if (m_spellInfo->SpellFamilyFlags[1] & 0x400)
+                            {
+                              damage = uint32(m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.8);
+                            }
                // Victory Rush
                else if (m_spellInfo->SpellFamilyFlags[1] & 0x100)
                {
