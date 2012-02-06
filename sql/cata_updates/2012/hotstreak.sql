7bd1ea42fb7a34d0c67672302d652b1413e73696
Remix CataWoW <remixcata@live.com>
2012-02-06 11:16:38 -0600

    Fix Hot streak

---
+++ sql/cata_updates/2012/hotstreak.sql
@@ -0,0 +1,3 @@
+INSERT INTO spell_script_names(spell_id,ScriptName)VALUES(11366,'spell_mage_pyroblast');
+INSERT INTO spell_linked_spell(spell_trigger,spell_effect,`type`,`comment`)
+VALUES(92315,-48108,0,'Pyroblast removes Hot Streak Aura');
