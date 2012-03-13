f930505b552b10b5123040e225b748a91909cab8
Remix99 <remixcata@live.com>
2012-03-13 00:04:53 -0500

    testinggggggggg

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -2589,30 +2589,6 @@ void Spell::SelectEffectTargets(uint32 i, uint32 cur)
                             }
                             break;
                         }
-				case 48018://Teach the tele spell on demonic circle use if they dont have it
-				{
-					if (!m_caster->HasSpell(35517))
-					{
-						m_caster->ToPlayer()->learnSpell(35517, true);//better tele spell
-						break;
-					}
-				}
-				case 35517:// Teleport - dummy spell. Demonic Circle hack
-				{
-					if (!m_caster->HasAura(48018))// tele spell
-					{
-						 m_caster->ToPlayer()->AddSpellCooldown(35517, 0, time(NULL) + 3);//cooldown the spell if used without demonic circle aura//prevent unwanted spamming
-					}
-					if (m_caster->HasAura(48018))// Demonic circle aura
-					{
-						m_caster->AddAura(48020, m_caster);//demonic circle: teleport has to be used as an aura to teleport the caster
-					}
-					if (m_caster->HasAura(74434))// has soul burn
-					{
-						m_caster->AddAura(79438, m_caster);
-					}
-					break;
-				}
                         // Corpse Explosion
                         case 49158:
                         case 51325:
@@ -3961,6 +3937,30 @@ void Spell::finish(bool ok)
                     }
                 }
                 break;
+				case 48018://Teach the tele spell on demonic circle use if they dont have it
+				{
+					if (!m_caster->HasSpell(35517))
+					{
+						m_caster->ToPlayer()->learnSpell(35517, true);//better tele spell
+						break;
+					}
+				}
+				case 35517:// Teleport - dummy spell. Demonic Circle hack
+				{
+					if (!m_caster->HasAura(48018))// tele spell
+					{
+						 m_caster->ToPlayer()->AddSpellCooldown(35517, 0, time(NULL) + 3);//cooldown the spell if used without demonic circle aura//prevent unwanted spamming
+					}
+					if (m_caster->HasAura(48018))// Demonic circle aura
+					{
+						m_caster->AddAura(48020, m_caster);//demonic circle: teleport has to be used as an aura to teleport the caster
+					}
+					if (m_caster->HasAura(74434))// has soul burn
+					{
+						m_caster->AddAura(79438, m_caster);
+					}
+					break;
+				}
             case 30455: // Ice Lance
             case 44572: // Deep Freeze
                 if (m_caster->HasAura(44544)) // Fingers of Frost
